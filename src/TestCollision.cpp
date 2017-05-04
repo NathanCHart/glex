a13d4b096f65edbbc37843ba64474755fd73460d
Nathan Hart <Nathan.Hart1@yahoo.co.uk>
2017-05-04 20:10:21 +0100

    Finish collision check of AABB

--- src/TestCollision.cpp
+++ src/TestCollision.cpp
@@ -1,14 +1,37 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE CollisionTest
 #include <boost/test/included/unit_test.hpp>
+#include <iostream>
+#include <memory>
+#include "AABBox.cc"
+#include "AABBox.h"
 
-int subtract(int x, int y)
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(Collision)
+
+//Test if the bounding box tests collision
+
+BOOST_AUTO_TEST_CASE(Collision1)
 {
-	return x-y;
+    auto b1 = make_shared<AABBox>(AABBox(Vector3(0.0f, 0.0f, 0.0f), 5.0f, 5.0f, 5.0f));
+    auto b2 = make_shared<AABBox>(AABBox(Vector3(2.5f, 2.5f, 2.5f), 5.0f, 5.0f, 5.0f));
+
+    BOOST_CHECK( b1->CollidesWith(b2) );
+    BOOST_CHECK( b2->CollidesWith(b1) );
 }
 
-BOOST_AUTO_TEST_CASE(subtractTest)
+//Test if the program can tell if an object is not colliding
+
+BOOST_AUTO_TEST_CASE(Collision2)
 {
-	BOOST_CHECK(subtract(5,5) == 0);
+    auto b1 = make_shared<AABBox>(AABBox(Vector3(0.0f, 0.0f, 0.0f), 5.0f, 5.0f, 5.0f));
+    auto b2 = make_shared<AABBox>(AABBox(Vector3(20.5f, 20.5f, 20.5f), 5.0f, 5.0f, 5.0f));
+
+    BOOST_CHECK( !b1->CollidesWith(b2) );
+    BOOST_CHECK( !b2->CollidesWith(b1) );
 }
+
+
+BOOST_AUTO_TEST_SUITE_END()
 
